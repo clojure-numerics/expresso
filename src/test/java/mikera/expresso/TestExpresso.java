c85f8b670f3053d897bf32cffd6745e72404e66b
mikera <mike.r.anderson.13@gmail.com>
2013-04-18 10:05:59 +0800

    add cljunit test

---
+++ src/test/java/mikera/expresso/TestExpresso.java
@@ -0,0 +1,11 @@
+package mikera.expresso;
+
+import mikera.cljunit.ClojureTest;
+
+public class TestExpresso extends ClojureTest {
+
+	@Override
+	public String filter() {
+		return "mikera.expresso";
+	}
+}
