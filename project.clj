dbb221d7e040e725ffd144c61fd79baca58c2879
Maik Schünemann <maikschuenemann@gmail.com>
2013-06-17 11:08:53 +0200

    updated project.clj

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.logic "0.8.3"]])
+                 [org.clojure/core.logic "0.8.3"]]
+  :src-paths ["src" "src/main/clojure"]
+  :test-paths ["src" "src/test/clojure"])
