471f8149605f539100909a8377feedce60dc537e
Maik Schünemann <maikschuenemann@gmail.com>
2013-07-15 08:27:36 +0200

    dependencies added

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.trace "0.7.5"]
                  [org.clojure/core.logic "0.8.3"]]
 
   :source-paths ["src" "src/main/clojure"]
