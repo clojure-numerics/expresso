5c8fae783aba90de8b0d2e3bf193ddc319af4302
mschuene <maikschuenemann@gmail.com>
2013-08-23 21:46:26 +0200

    fix build erroring

--- src/test/java/mikera/expresso/TestExpresso.java
+++ src/test/java/mikera/expresso/TestExpresso.java
@@ -3,8 +3,6 @@ package mikera.expresso;
 import mikera.cljunit.ClojureTest;
 
 public class TestExpresso extends ClojureTest {
-
-	@Override
 	public String filter() {
 		return "numeric.expresso";
 	}
