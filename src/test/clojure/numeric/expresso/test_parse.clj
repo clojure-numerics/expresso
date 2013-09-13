860afb0f3eb5ba634082a870643b8cac190c7590
mikera <mike.r.anderson.13@gmail.com>
2013-09-13 17:06:58 +0800

    extra parse tests

--- src/test/clojure/numeric/expresso/test_parse.clj
+++ src/test/clojure/numeric/expresso/test_parse.clj
@@ -19,3 +19,10 @@
   (is (= (ex (+ (* 1 2 3) 4)) (parse-expression "1*2*3+4")))
   (is (= (ex (* 1 2 (+ 3 4))) (parse-expression "1*2*(3+4)")))
   (is (= (ex (+ 1 (* 2 (numeric.expresso.core/** 3 4)) 5)) (parse-expression "1+2*3**4+5"))))
+
+(deftest test-parse-variables
+  (is (= (ex (+ x 1)) (parse-expression "x+1"))))
+
+(deftest test-parse-function
+  (is (= (ex (abs x)) (parse-expression "abs(x)"))))
+
