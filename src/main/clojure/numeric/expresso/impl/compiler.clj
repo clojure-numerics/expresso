3d84a552bf0020b34f34187b2305c04e0615835e
Maik Schünemann <maikschuenemann@gmail.com>
2014-01-24 11:40:52 +0100

    -

--- src/main/clojure/numeric/expresso/impl/compiler.clj
+++ src/main/clojure/numeric/expresso/impl/compiler.clj
@@ -68,7 +68,7 @@
 (defmethod compile-pattern '== [[_ path eq]]
   `(==patho (utils/get-in-expression ~'expr ~(compile-path path)) ~eq ~(compile-path path)))
 
-(defmethod compile-pattern 'count [[_ path  eq]]
+(defmethod compile-pattern 'count [[_ path eq]]
   `(== (let [~'s (utils/get-in-expression ~'expr ~(compile-path path))]
            (and (sequential? ~'s) (count ~'s))) (+ 1 ~eq)))
 
@@ -87,16 +87,6 @@
 (defn to-constr [lv]
   `(lvar ~(list 'quote (symbol (:name lv))) false))
 
-(def ^:dynamic *compile-rules* 1)
-
-(defn get-compile-rules [] *compile-rules*)
-
-(defn apply-chosen-rule [i q]
-  (let [rule (nth r i)]
-    (all
-     (check-guardo (nth rule 2))
-     (apply-transformationo (second rule) q))))
-
 (defmethod compile-pattern 'finalize-match [[_ rule i]]
   (let [[pat trans guard] rule
         lv-pos (lvar-positions pat)]
@@ -105,32 +95,31 @@
                        `(== ~(to-constr lv)
                             (utils/get-in-expression ~'expr ~(compile-path pos))))
                      lv-pos)
-                [`(apply-chosen-rule ~i ~'q )]))))
-                ;[`(check-guardo (nth (nth ~'r ~i) 2))
-                 ;`(apply-transformationo (second (nth ~'r ~i)) ~'q)]))))
+                [`(check-guardo (nth (nth ~'r ~i) 2))
+                 `(apply-transformationo (second (nth ~'r ~i)) ~'q)]))))
       
 
 ;;IDEA to avoid issues with core.logics lack of environment trimming add the bindings , then do the transformations and then delete! the bindings again from the substitution
 (defn rules-to-code [rules]
   (->> rules
       walk/macroexpand-all
-;       (map first)
-;       (#(do (prn %) %))
+; (map first)
+; (#(do (prn %) %))
       to-decision-tree
-     ; (#(do (prn %) %))
+      (#(do (prn %) %))
       (transform-expression dt-rules)
-     ; (#(do (prn %) %))
-      compile-pattern))
-
+      (#(do (prn %) %))
+       compile-pattern))
 
 (defn compile-rules [rules]
-  (let []
+  (let [code (rules-to-code rules)
+        c `(fn [~'expr ~'q]
+             ~(rules-to-code rules))
+        _ (prn c)]
     ;;metadata is evil when there are functions in it
     ;;so strip the metadata here. In future a dedicated
     ;;function should do it so that one doesn't need a reader
-    (binding [*compile-rules* rules]
-              (eval (read-string (str `(fn [~'expr ~'q]
-             ~(rules-to-code rules))))))))
+    (eval (read-string (str c)))))
 
 
 (defn test-hacko []
@@ -140,3 +129,131 @@
       (set-partial-match a
                          (assoc pm :succeed-1 "yes")))))
 
+(defn fl [expr q]
+  (clojure.core.logic/conde
+ ((clojure.core.logic/==
+   (clojure.core/let
+    [s (numeric.expresso.utils/get-in-expression expr [])]
+    (clojure.core/and
+     (clojure.core/sequential? s)
+     (clojure.core/first s)))
+   '+)
+  (clojure.core.logic/==
+   (clojure.core/let
+    [s (numeric.expresso.utils/get-in-expression expr [])]
+    (clojure.core/and
+     (clojure.core/sequential? s)
+     (clojure.core/count s)))
+   (clojure.core/+ 1 2))
+  (clojure.core.logic/conde
+   ((clojure.core.logic/all
+     (clojure.core.logic/==
+      (clojure.core.logic/lvar '?a false)
+      (numeric.expresso.utils/get-in-expression expr [0]))
+     (clojure.core.logic/==
+      (clojure.core.logic/lvar '?b false)
+      (numeric.expresso.utils/get-in-expression expr [1]))
+     (numeric.expresso.rules/check-guardo
+      (clojure.core/nth (clojure.core/nth r 0) 2))
+     (numeric.expresso.rules/apply-transformationo
+      (clojure.core/second (clojure.core/nth r 0))
+      q)))
+   ((numeric.expresso.impl.compiler/==patho
+     (numeric.expresso.utils/get-in-expression
+ expr [1])
+     0
+     [1])
+    (clojure.core.logic/all
+     (clojure.core.logic/==
+      (clojure.core.logic/lvar '?a false)
+      (numeric.expresso.utils/get-in-expression expr [0]))
+     (numeric.expresso.rules/check-guardo
+      (clojure.core/nth (clojure.core/nth r 2) 2))
+     (numeric.expresso.rules/apply-transformationo
+      (clojure.core/second (clojure.core/nth r 2))
+      q)))))
+ ((clojure.core.logic/==
+   (clojure.core/let
+    [s (numeric.expresso.utils/get-in-expression expr [])]
+    (clojure.core/and
+     (clojure.core/sequential? s)
+     (clojure.core/first s)))
+   '*)
+  (clojure.core.logic/==
+   (clojure.core/let
+    [s (numeric.expresso.utils/get-in-expression expr [])]
+    (clojure.core/and
+     (clojure.core/sequential? s)
+     (clojure.core/count s)))
+   (clojure.core/+ 1 2))
+  (clojure.core.logic/conde
+   ((clojure.core.logic/all
+     (clojure.core.logic/==
+      (clojure.core.logic/lvar '?a false)
+      (numeric.expresso.utils/get-in-expression expr [0]))
+     (clojure.core.logic/==
+      
+(clojure.core.logic/lvar '?b false)
+      (numeric.expresso.utils/get-in-expression expr [1]))
+     (numeric.expresso.rules/check-guardo
+      (clojure.core/nth (clojure.core/nth r 1) 2))
+     (numeric.expresso.rules/apply-transformationo
+      (clojure.core/second (clojure.core/nth r 1))
+      q)))
+   ((numeric.expresso.impl.compiler/==patho
+     (numeric.expresso.utils/get-in-expression expr [0])
+     0
+     [0])
+    (clojure.core.logic/all
+     (clojure.core.logic/==
+      (clojure.core.logic/lvar '?a false)
+      (numeric.expresso.utils/get-in-expression expr [1]))
+     (numeric.expresso.rules/check-guardo
+      (clojure.core/nth (clojure.core/nth r 3) 2))
+     (numeric.expresso.rules/apply-transformationo
+      (clojure.core/second (clojure.core/nth r 3))
+      q)))
+   ((numeric.expresso.impl.compiler/==patho
+     (numeric.expresso.utils/get-in-expression expr [0])
+     1
+     [0])
+    (clojure.core.logic/all
+     (clojure.core.logic/==
+      (clojure.core.logic/lvar '?a false)
+      (numeric.expresso.utils/get-in-expression
+ expr [1]))
+     (numeric.expresso.rules/check-guardo
+      (clojure.core/nth (clojure.core/nth r 4) 2))
+     (numeric.expresso.rules/apply-transformationo
+      (clojure.core/second (clojure.core/nth r 4))
+      q)))
+   ((clojure.core.logic/==
+     (clojure.core/let
+      [s (numeric.expresso.utils/get-in-expression expr [1])]
+      (clojure.core/and
+       (clojure.core/sequential? s)
+       (clojure.core/first s)))
+     '+)
+    (clojure.core.logic/==
+     (clojure.core/let
+      [s (numeric.expresso.utils/get-in-expression expr [1])]
+      (clojure.core/and
+       (clojure.core/sequential? s)
+       (clojure.core/count s)))
+     (clojure.core/+ 1 2))
+    (clojure.core.logic/all
+     (clojure.core.logic/==
+      (clojure.core.logic/lvar '?a false)
+      (numeric.expresso.utils/get-in-expression expr [0]))
+     (clojure.core.logic/==
+      (clojure.core.logic/lvar '?b false)
+      (numeric.expresso.utils/get-in-expression expr [1 0]))
+     (clojure.core.logic/==
+      (clojure.core.logic/lvar '?c false)
+      
+(numeric.expresso.utils/get-in-expression expr [1 1]))
+     (numeric.expresso.rules/check-guardo
+      (clojure.core/nth (clojure.core/nth r 5) 2))
+     (numeric.expresso.rules/apply-transformationo
+      (clojure.core/second (clojure.core/nth r 5))
+      q)))))))
