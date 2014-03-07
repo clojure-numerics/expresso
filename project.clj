287d58d028e3efb81e0ecdb29d40ca13c4c5b20f
Maik Schünemann <maikschuenemann@gmail.com>
2014-03-07 15:28:44 +0100

    changed license to MIT

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject expresso "0.2.1-SNAPSHOT"
   :description "a general Algebraic Expression manipulation library in clojure"
   :url "https://github.com/clojure-numerics/expresso"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/mit-license.php"}
   :profiles {:dev {:dependencies [[org.clojure/tools.trace "0.7.6"]
                                   [criterium "0.4.2"]]}}
   :dependencies [[org.clojure/clojure "1.5.1"]
@@ -12,7 +12,5 @@
                  [org.clojure/core.logic "0.8.4"]]
   :source-paths ["src" "src/main/clojure"]
   :test-paths ["test" "src/test/clojure"]
-  :aot [numeric.expresso.impl.pimplementation]
-
-)
+  :aot [numeric.expresso.impl.pimplementation])
 
