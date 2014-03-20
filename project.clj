255bd2dc21fb3a32e2f7ad6296586d810843b18a
Maik Schünemann <maikschuenemann@gmail.com>
2014-03-20 20:07:37 +0100

    updated dependencies

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "https://github.com/clojure-numerics/expresso"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/mit-license.php"}
-  :profiles {:dev {:dependencies [[org.clojure/tools.trace "0.7.6"]
-                                  [criterium "0.4.2"]]}}
+  :profiles {:dev {:dependencies [[org.clojure/tools.trace "0.7.8"]
+                                  [criterium "0.4.3"]]}}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [instaparse "1.2.3"]
+                 [instaparse "1.3.0"]
                  [org.clojure/core.memoize "0.5.6"]
-                 [net.mikera/core.matrix "0.19.0"]
-                 [org.clojure/core.logic "0.8.4"]]
+                 [net.mikera/core.matrix "0.20.0"]
+                 [org.clojure/core.logic "0.8.7"]]
   :source-paths ["src" "src/main/clojure"]
   :test-paths ["test" "src/test/clojure"]
   :aot [numeric.expresso.impl.pimplementation])
