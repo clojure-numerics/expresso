6ca22f639b73458fe597f88dbb0a415bc77c13bf
Maik Schünemann <maikschuenemann@gmail.com>
2015-03-24 13:12:00 +0100

    update dependencies

--- project.clj
+++ project.clj
@@ -5,11 +5,11 @@
             :url "http://opensource.org/licenses/mit-license.php"}
   :profiles {:dev {:dependencies [[org.clojure/tools.trace "0.7.8"]
                                   [criterium "0.4.3"]]}}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [instaparse "1.3.0"]
                  [org.clojure/core.memoize "0.5.6"]
-                 [net.mikera/core.matrix "0.20.0"]
-                 [org.clojure/core.logic "0.8.7"]]
+                 [net.mikera/core.matrix "0.33.2"]
+                 [org.clojure/core.logic "0.8.10"]]
   :source-paths ["src" "src/main/clojure"]
   :test-paths ["test" "src/test/clojure"]
   :aot [numeric.expresso.impl.pimplementation])
