bccc275861a4b4fd75c5c6e3c770970cb0a78946
Maik Schünemann <maikschuenemann@gmail.com>
2015-06-19 12:45:26 +0200

    bump version

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject expresso "0.2.1-SNAPSHOT"
+(defproject expresso "0.2.2-SNAPSHOT"
   :description "a general Algebraic Expression manipulation library in clojure"
   :url "https://github.com/clojure-numerics/expresso"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/mit-license.php"}
   :profiles {:dev {:dependencies [[org.clojure/tools.trace "0.7.8"]
                                   [criterium "0.4.3"]]}}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [instaparse "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.7.0-RC2"]
+                 [instaparse "1.4.0"]
                  [org.clojure/core.memoize "0.5.6"]
-                 [net.mikera/core.matrix "0.33.2"]
+                 [net.mikera/core.matrix "0.36.0"]
                  [org.clojure/core.logic "0.8.10"]]
   :source-paths ["src" "src/main/clojure"]
   :test-paths ["test" "src/test/clojure"]
